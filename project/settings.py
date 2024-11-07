cfb1f314d4ec62bce58df8abce6c32ad6a09cd20
hamzakhalem <hamzakhalem97@gmail.com>
2024-11-07 14:36:48 +0100

    inmodels

--- project/settings.py
+++ project/settings.py
@@ -37,8 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'tickets',
 ]
+
+REST_FRAMEWORK = {}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -51,6 +54,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'project.urls'
+
+
 
 TEMPLATES = [
     {
