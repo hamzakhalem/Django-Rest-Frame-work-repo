75d665b7b7ae00a37fb1bc2ba3df50a8e74e38c7
hamzakhalem <hamzakhalem97@gmail.com>
2024-11-08 19:05:31 +0100

    inmodels

--- tickets/views.py
+++ tickets/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.http.response import JsonResponse
+from .models import Guest, Movie, Reservation
+
 
 # Create your views here.
+#1 without REST and no model query FBV
+def no_rest_no_model(request):
+    guests = [
+        {
+            'id': 1,
+            "Name": "Omar",
+            "mobile": 789456,
+        },
+        {
+            'id': 2,
+            'name': "yassin",
+            'mobile': 74123,
+        }
+    ]
+    return JsonResponse (guests, safe=False)
+
+def no_rest_from_model(request):
+    data = Guest.objects.all()
+    response = {
+        'guests': list(data.values('name','mobile'))
+    }
+    return JsonResponse(response)
