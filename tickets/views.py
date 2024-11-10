bca235350b28e615998f7f6067175bef0f35aadd
hamzakhalem <hamzakhalem97@gmail.com>
2024-11-10 16:27:34 +0100

    views

--- tickets/views.py
+++ tickets/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http.response import JsonResponse
+from rest_framework.response import Response
 from .models import Guest, Movie, Reservation
-
+from rest_framework.decorators import api_view
+from .serializers import GuestSerializer, MovieSerializer, ReservationSerializer
+from rest_framework import status, filters
 
 # Create your views here.
 #1 without REST and no model query FBV
@@ -26,3 +29,18 @@ def no_rest_from_model(request):
         'guests': list(data.values('name','mobile'))
     }
     return JsonResponse(response)
+
+
+def FBV_List(request):
+    # GET
+    if request.method == 'GET':
+        guests = Guest.objects.all()
+        serializer = GuestSerializer(guests, many=True)
+        return Response(serializer.data)
+    # POST
+    elif request.method == 'POST':
+        serializer = GuestSerializer(data= request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status= status.HTTP_201_CREATED)
+        return Response(serializer.data, status= status.HTTP_400_BAD_REQUEST)
